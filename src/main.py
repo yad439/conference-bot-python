bcf1e6fa5dedc24ebf5caddd03294d0170063e42
Vedblade <18503283+yad439@users.noreply.github.com>
2025-04-12 12:52:17 +0000

    Add scheduler startup configuration

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 from aiogram import Bot, Dispatcher
+from apscheduler.schedulers.asyncio import AsyncIOScheduler  # pyright: ignore[reportMissingTypeStubs]
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 import data.mock_data
@@ -10,6 +11,7 @@ import handlers.general
 import handlers.personal_edit
 import handlers.personal_view
 from data.repository import Repository
+from notifications import event_start
 
 
 async def main():
@@ -22,12 +24,18 @@ async def main():
     session_maker = async_sessionmaker(engine)
     await data.setup.create_tables(engine)
     await data.mock_data.fill_tables(session_maker)
-    speech_repository = Repository(session_maker)
+    repository = Repository(session_maker)
+
     bot = Bot(token)
-    dispatcher = Dispatcher(repository=speech_repository)
+    dispatcher = Dispatcher(repository=repository)
     dispatcher.include_router(handlers.general.get_router())
     dispatcher.include_router(handlers.personal_view.get_router())
     handlers.personal_edit.init(dispatcher)
+
+    scheduler = AsyncIOScheduler()
+    await event_start.configure_events(scheduler, repository, bot, 5)
+    scheduler.start()
+
     logging.info('Starting polling')
     await dispatcher.start_polling(bot)  # type: ignore
 
